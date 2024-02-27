bf335cd86a2c1725489adc7d4596699efc6edc3b
Enes <9263360+EnessenE@users.noreply.github.com>
2024-02-27 21:45:43 +0100

    More UI work

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -10,21 +10,45 @@ import { ApiService } from '../../services/api.service';
 })
 export class MainComponent implements OnInit {
   foundStops: string[] | undefined;
+  foundStopsOrigin: string[] | undefined;
+  originStop: string = 'Amsterdam Centraal';
+  destinationStop: string = 'Eindhoven Centraal';
+  possibility: string | undefined;
 
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {}
 
-  SearchResult(stops: string[]) {
-    this.foundStops = stops;
-    console.log('new data loaded for stops');
-  }
-
   async onSearchInputChange(event: any) {
     var searchText = event.target.value;
     console.log('Searching for ' + searchText);
     this.apiService.GetStops(event.target.value).subscribe({
-      next: (data) => this.SearchResult(data),
+      next: (data) => (this.foundStops = data),
+    });
+  }
+
+  async onSearchInputChangeOrigin(event: any) {
+    var searchText = event.target.value;
+    console.log('Searching for ' + searchText);
+    this.apiService.GetStops(event.target.value).subscribe({
+      next: (data) => (this.foundStopsOrigin = data),
+    });
+  }
+
+  selectStopOrigin(value: string) {
+    this.originStop = value;
+    this.checkPossibility();
+  }
+
+  selectStopDestination(value: string) {
+    this.destinationStop = value;
+    this.checkPossibility();
+  }
+
+  checkPossibility() {
+    console.log(this.originStop + ' > ' + this.destinationStop);
+    this.apiService.GetPossibility(this.originStop, this.destinationStop).subscribe({
+      next: (data) => (this.possibility = data),
     });
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,12 +11,17 @@ export class ApiService {
 
   GetStops(text: string): Observable<Array<string>> {
     if (!(text.length > 0)) {
-      text = "Amsterdam"
+      text = 'Amsterdam';
     }
 
-      return this.http.get<Array<string>>(
-        environment.apiServer + '/stops/search?filter=' + text
-      );
-    
+    return this.http.get<Array<string>>(
+      environment.apiServer + '/stops/search?filter=' + text
+    );
+  }
+
+  GetPossibility(origin: string, destination: string): Observable<string> {
+    return this.http.get<string>(
+      environment.apiServer + '/Disruption/' + origin + '/' + destination
+    );
   }
 }
