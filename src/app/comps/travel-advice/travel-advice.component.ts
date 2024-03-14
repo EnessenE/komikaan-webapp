2ed248e9fedbc812a06f4bdd869fd0cb8151c664
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-14 18:48:55 +0100

    Add expansion to travel-advice and pin icons

--- src/app/comps/travel-advice/travel-advice.component.ts
+++ src/app/comps/travel-advice/travel-advice.component.ts
@@ -2,15 +2,20 @@ import { Component, Input, input } from '@angular/core';
 import { TravelAdvice } from '../../models/journey-result';
 import { DatePipe } from '@angular/common';
 import { NgbAccordionModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { TravelAdviceRouteComponent } from '../travel-advice-route/travel-advice-route.component';
+import { TravelAdviceExpandedComponent } from '../travel-advice-expanded/travel-advice-expanded.component';
+import { IconsModule } from '../../icons/icons.module';
 
 @Component({
     selector: 'app-travel-advice',
     standalone: true,
-    imports: [DatePipe, NgbTooltipModule, NgbAccordionModule],
+    imports: [DatePipe, NgbTooltipModule, NgbAccordionModule, TravelAdviceRouteComponent, TravelAdviceExpandedComponent, IconsModule],
     templateUrl: './travel-advice.component.html',
     styleUrl: './travel-advice.component.scss',
 })
 export class TravelAdviceComponent {
     @Input()
     travelAdvice!: TravelAdvice;
+    @Input()
+    index!: number;
 }
