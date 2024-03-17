3abbc0c3dc30c64975db0b200a244d2955e63311
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-17 15:49:43 +0100

    Time sections

--- src/app/comps/travel-advice-expanded/travel-advice-expanded.component.ts
+++ src/app/comps/travel-advice-expanded/travel-advice-expanded.component.ts
@@ -3,11 +3,12 @@ import { TravelAdvice } from '../../models/journey-result';
 import { DatePipe } from '@angular/common';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { TrackSelectionComponent } from '../track-selection/track-selection.component';
+import { TimeSelectionComponent } from '../time-selection/time-selection.component';
 
 @Component({
   selector: 'app-travel-advice-expanded',
   standalone: true,
-  imports: [DatePipe, NgbTooltipModule, TrackSelectionComponent],
+  imports: [DatePipe, NgbTooltipModule, TrackSelectionComponent, TimeSelectionComponent],
   templateUrl: './travel-advice-expanded.component.html',
   styleUrl: './travel-advice-expanded.component.scss'
 })
--- src/app/comps/travel-advice-route/travel-advice-route.component.ts
+++ src/app/comps/travel-advice-route/travel-advice-route.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
-import { RoutePart, TravelAdvice } from '../../models/journey-result';
+import { TravelAdvice } from '../../models/journey-result';
 import { DatePipe } from '@angular/common';
 import { NgbTooltipModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { TrackSelectionComponent } from '../track-selection/track-selection.component';
+import { TimeSelectionComponent } from '../time-selection/time-selection.component';
 
 @Component({
     selector: 'app-travel-advice-route',
     standalone: true,
-    imports: [DatePipe, NgbTooltipModule, NgbAccordionModule, TrackSelectionComponent],
+    imports: [DatePipe, NgbTooltipModule, NgbAccordionModule, TrackSelectionComponent, TimeSelectionComponent],
     templateUrl: './travel-advice-route.component.html',
     styleUrl: './travel-advice-route.component.scss',
 })
