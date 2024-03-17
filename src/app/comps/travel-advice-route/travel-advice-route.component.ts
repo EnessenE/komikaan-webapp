fddac2b2b8df8cdc1d92e04979d050134f52d3d3
Enes <9263360+EnessenE@users.noreply.github.com>
2024-03-17 14:59:03 +0100

    Make use of TrackSelectionComponent

--- src/app/comps/travel-advice-route/travel-advice-route.component.ts
+++ src/app/comps/travel-advice-route/travel-advice-route.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { RoutePart, TravelAdvice } from '../../models/journey-result';
 import { DatePipe } from '@angular/common';
 import { NgbTooltipModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { TrackSelectionComponent } from '../track-selection/track-selection.component';
 
 @Component({
     selector: 'app-travel-advice-route',
     standalone: true,
-    imports: [DatePipe, NgbTooltipModule, NgbAccordionModule],
+    imports: [DatePipe, NgbTooltipModule, NgbAccordionModule, TrackSelectionComponent],
     templateUrl: './travel-advice-route.component.html',
     styleUrl: './travel-advice-route.component.scss',
 })
