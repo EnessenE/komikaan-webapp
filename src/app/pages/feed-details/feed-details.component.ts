4188e81d8d7b4d6cb9e2aafdc98553509fb7e758
Enes <9263360+EnessenE@users.noreply.github.com>
2024-12-24 22:29:39 +0100

    Upgrade steps and import attempt fixes

--- src/app/pages/feed-details/feed-details.component.ts
+++ src/app/pages/feed-details/feed-details.component.ts
@@ -9,10 +9,12 @@ import { GTFSSearchStop } from '../../models/gtfssearchstop';
 import { VehiclePosition } from '../../models/vehicle-position';
 import { LeafletMarkerClusterModule } from '@bluehalo/ngx-leaflet-markercluster';
 import { LeafletModule, LeafletControlLayersConfig } from '@bluehalo/ngx-leaflet';
+import 'leaflet.markercluster';
+import L from 'leaflet';
 
 @Component({
     selector: 'app-feed-details',
-    imports: [RouterLink, LeafletModule, LeafletMarkerClusterModule, RouteComponent],
+    imports: [LeafletModule, LeafletMarkerClusterModule],
     templateUrl: './feed-details.component.html',
     styleUrl: './feed-details.component.scss'
 })
@@ -32,41 +34,43 @@ export class FeedDetailsComponent implements OnInit {
     };
 
     mapFitToBounds!: LatLngBounds;
-    
+
     markerClusterOptions: MarkerClusterGroupOptions = {};
-    clusterGroup: MarkerClusterGroup = new MarkerClusterGroup({
-      spiderfyOnMaxZoom: false,
-      showCoverageOnHover: true,
-      zoomToBoundsOnClick: true,
-      disableClusteringAtZoom: 15,
-      removeOutsideVisibleBounds: true,
-      animate: true,
-      chunkedLoading: true,
-    });;
+    clusterGroup: MarkerClusterGroup;
 
     options = {
         zoom: 13,
         center: latLng(52.0907, 5.1214),
     };
 
-    layers: Layer[] = [
-        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        }),
-    ];
+    layers: Layer[];
 
     constructor(
         private apiService: ApiService,
         private route: ActivatedRoute,
         private titleService: Title,
-    ) {}
-
-    ngOnInit(): void {
-        this.loading = true;
+    ) {
+        console.log('MarkerClusterGroup is loaded:', L.MarkerClusterGroup);
 
         this.layers = [
             tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             }),
         ];
+        
+        this.clusterGroup = new MarkerClusterGroup({
+            spiderfyOnMaxZoom: false,
+            showCoverageOnHover: true,
+            zoomToBoundsOnClick: true,
+            disableClusteringAtZoom: 15,
+            removeOutsideVisibleBounds: true,
+            animate: true,
+            chunkedLoading: true,
+        });
+    }
+
+    ngOnInit(): void {
+        this.loading = true;
+
         this.markerLayers?.eachLayer((layer) => {
             this.markerLayers.removeLayer(layer);
         });
@@ -103,7 +107,7 @@ export class FeedDetailsComponent implements OnInit {
     addStopsToMap(stops: GTFSSearchStop[]) {
         stops.forEach((stop) => {
             stop.adjustedCoordinates.forEach((coordinate) => {
-                var stopLayer = circle([coordinate.latitude, coordinate.longitude], { radius: 100 });
+                var stopLayer = circle([coordinate.latitude, coordinate.longitude], { radius: 25 });
 
                 var popup = new Popup();
                 popup.setContent('<a href="/stops/' + stop.id + '/' + stop.stopType + '">' + stop.name + '</a>');
@@ -124,12 +128,12 @@ export class FeedDetailsComponent implements OnInit {
 
     addVehiclesToMap(vehicles: VehiclePosition[]) {
         vehicles.forEach((vehicle) => {
-                var stopLayer = circle([vehicle.latitude, vehicle.longitude], { radius: 100, color: "green" });
+            var stopLayer = circle([vehicle.latitude, vehicle.longitude], { radius: 100, color: "green" });
 
-                var popup = new Popup();
-                popup.setContent(vehicle.id);
-                stopLayer.bindPopup(popup);
-                this.clusterGroup.addLayer(stopLayer);
+            var popup = new Popup();
+            popup.setContent(vehicle.id);
+            stopLayer.bindPopup(popup);
+            this.clusterGroup.addLayer(stopLayer);
         });
         // Timeout due to timing bug on the initalization for an unknown reason.
         setTimeout(() => {
